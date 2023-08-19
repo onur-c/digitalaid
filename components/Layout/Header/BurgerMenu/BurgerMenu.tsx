106eb80764a659e06be0128a9511cf8b47d651cb
Onur Celik <60694762+onur-c@users.noreply.github.com>
2023-08-19 17:52:34 +0100

    burger menu and some color changes

--- components/Layout/Header/BurgerMenu/BurgerMenu.tsx
+++ components/Layout/Header/BurgerMenu/BurgerMenu.tsx
@@ -1,8 +1,9 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { links } from "../links";
+import { IoMdArrowDropleft } from "react-icons/io";
 const BurgerMenu = ({
   isOpen,
   handleBurgerMenu,
@@ -10,6 +11,10 @@ const BurgerMenu = ({
   isOpen: boolean;
   handleBurgerMenu: () => void;
 }) => {
+  const [isDropdown, setDropdown] = useState<boolean>(false);
+  const handleDropdown = () => {
+    setDropdown(!isDropdown);
+  };
   return (
     <div className="burger-menu">
       <div
@@ -25,13 +30,36 @@ const BurgerMenu = ({
             : "invisible absolute transition-all right-10 opacity-0"
         }
       >
-        {links.map((link) => (
-          <Link href={link.url} key={link.id} onClick={handleBurgerMenu}>
-            <p className="text-lg  h-1/5 font-light ">
-              {link.title}
-            </p>
+        {links.slice(0, 3).map((link) => (
+          <Link href={link.url} key={link.id}>
+            <p className="hover:opacity-60">{link.title}</p>
           </Link>
         ))}
+        <div
+          onClick={handleDropdown}
+          className={isDropdown ? " cursor-pointer " : " cursor-pointer "}
+        >
+          <p className="flex items-center right-4 relative hover:opacity-60">
+            <IoMdArrowDropleft />
+            Hizmetler
+          </p>
+          <div
+            className={
+              isDropdown
+                ? "absolute flex gap-5 top-2/3 rounded-lg right-32 flex-col bg-dark p-4"
+                : "hidden"
+            }
+          >
+            {links.slice(3, 6).map((link) => (
+              <Link href={link.url} key={link.id}>
+                <p className="hover:opacity-60">{link.title}</p>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <Link href={links[6].url} key={links[6].id}>
+          <p className="hover:opacity-60">{links[6].title}</p>
+        </Link>
       </div>
     </div>
   );
